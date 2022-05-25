c2f68da062e6b5459dcd54afe53cfa2ddaa1f44e
Victor Tilve <ing.victortilve@gmail.com>
2022-05-25 11:53:42 -0500

    ejercicio section 5

---
+++ Understanding-TS/seciton5/src/app.ts
@@ -0,0 +1,12 @@
+class Deparment {
+    name:string;
+    
+    constructor(name:string) {
+        this.name = name;
+    
+    }
+}
+
+const account = new Deparment('Accounting');
+
+console.log(account);
